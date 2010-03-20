ed189d225021508032053266688a67dbb14b9296
suakx <github@suakx.com>
2010-03-21 06:16:58 +0900

    add tag

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 R3bAuthtest::Application.routes.draw do |map|
   # map.resource :account, :controller => "users"
   resource :account, :controller => "users"
+  
   # map.resources :password_resets
   resources :password_resets
+  
   # map.resources :users
   resources :users
+  
   # map.resource :user_session
   resource :user_session
+  
   # map.root :controller => "user_sessions", :action => "new" # optional, this just sets the root route
   root :to => "user_sessions#new"
 end
