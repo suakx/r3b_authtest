f8ba2898d9f155ad80d02a58c3ede08a855dca6b
suakx <github@suakx.com>
2010-03-22 18:32:16 +0900

    login logout

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,11 @@ R3bAuthtest::Application.routes.draw do |map|
   # map.resource :user_session
   resource :user_session
   
+  match "/register" => "users#new", :as => 'register'
+  match "/login" => "user_sessions#new", :as => 'login'
+  match "/logout" => "user_sessions#destroy", :as => 'logout'
+  match "/preferences" => "users#show", :as => 'preferences'
+  
   # map.root :controller => "user_sessions", :action => "new" # optional, this just sets the root route
-  root :to => "user_sessions#new"
+  root :to => "users#new"
 end
