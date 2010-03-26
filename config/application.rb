546e8bae647430bb5f9e05715a01f965162f977c
suakx <github@suakx.com>
2010-03-26 15:36:52 +0900

    clean

--- config/application.rb
+++ config/application.rb
@@ -37,10 +37,6 @@ module R3bAuthtest
     # end
 
     # Configure sensitive parameters which will be filtered from the log file.
-    
-    
-    # config.filter_parameters << :password
-    # filter_parameter_logging :password, :password_confirmation
     config.filter_parameters << :password << :password_confirmation
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 R3bAuthtest::Application.routes.draw do |map|
-  # map.resource :account, :controller => "users"
   resource :account, :controller => "users"
-  
-  # map.resources :password_resets
   resources :reset_password
-  
-  # map.resources :users
   resources :users
-  
-  # map.resource :user_session
   resource :user_session
   
   match "/register" => "users#new", :as => 'register'
@@ -17,7 +10,6 @@ R3bAuthtest::Application.routes.draw do |map|
   match "/preferences" => "users#show", :as => 'preferences'
   match "/forgot_password" => "reset_passwords#new", :as => 'forgot_password'
   
-  # map.root :controller => "user_sessions", :action => "new" # optional, this just sets the root route
   root :to => "users#new"
   match ':controller(/:action(/:id(.:format)))'
 end
