b8a7230a3f32cd8e84701f94f6d790fcc50d9705
suakx <github@suakx.com>
2010-03-27 13:36:55 +0900

    fix some

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 class Notifier < ActionMailer::Base
-  default_url_options[:host] = "192.168.0.18:3000"
-  default :from => "donotreply@suakx.com"
+  default_url_options[:host] = "lucid:3000"
+  default :from => "noreply@suakx.com"
 
   def reset_password_instructions(user)
     @user = user
